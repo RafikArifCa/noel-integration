0c05298794a8e5bfc3d3d0a090998376d540dcb5
Rafik <arifrafik@gmail.com>
2022-12-23 13:41:05 -0500

    #1 commit add files

--- src/test/java/com/rafik/reactspring/ReactSpringApplicationTests.java
+++ src/test/java/com/rafik/reactspring/ReactSpringApplicationTests.java
@@ -1,13 +1,20 @@
 package com.rafik.reactspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReactSpringApplicationTests {
+	public static Logger log = LoggerFactory.getLogger(ReactSpringApplication.class);
 
 	@Test
 	void contextLoads() {
+		log.info("test contextLoads...");
+		assertEquals(true, true);
 	}
 
 }
