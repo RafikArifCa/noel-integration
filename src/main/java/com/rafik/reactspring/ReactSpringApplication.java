e847b11bd113114d3850cde2dc5ce59c616dfcb7
Rafik <arifrafik@gmail.com>
2022-12-23 13:36:50 -0500

    the postconstruct method

--- src/main/java/com/rafik/reactspring/ReactSpringApplication.java
+++ src/main/java/com/rafik/reactspring/ReactSpringApplication.java
@@ -1,13 +1,23 @@
 package com.rafik.reactspring;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactSpringApplication {
+	public static Logger log = LoggerFactory.getLogger(ReactSpringApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactSpringApplication.class, args);
+	}
+
+	@PostConstruct
+	public static void init() {
+		log.info("init ... !!! ... !!! ...");
 	}
 
 }
